8c2a94d2d1fba0af3f441c88a8fb82a3875400ae
J. Matt Peterson <jmatt@jmatt.org>
2015-09-05 20:20:52 -0700

    Template for the user_verify script.
    Initial experimental ssh_server.
    
    	new file:   gar/__init__.py
    	new file:   gar/ssh_server.py
    	new file:   scripts/user_verify.py

---
+++ scripts/user_verify.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import argparse
+import getpass
+import libsaas
+
+class PasswordPromptAction(argparse.Action):
+    def __init__(self,
+                 option_strings,
+                 dest=None,
+                 nargs=0,
+                 default=None,
+                 required=False,
+                 type=None,
+                 metavar=None,
+                 help=None):
+        super(PasswordPromptAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            nargs=nargs,
+            default=default,
+            required=required,
+            metavar=metavar,
+            type=type,
+            help=help)
+
+    def __call__(self, parser, args, values, option_string=None):
+        password = getpass.getpass()
+        setattr(args, self.dest, password)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', required=True,
+                        help='Github user to verify.')
+    parser.add_argument('-p', '--password', required=True,
+                        action=PasswordPromptAction, type=str,
+                        help='Github user password.')
+    parser.add_argument('-r', '--repo', required=True,
+                        help='Repository to verify.')
+    
+
+if __name__ == '__main__':
+    main()
