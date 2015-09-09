a4489c5968b84277b191c0d6322b12edc28c79c9
J. Matt Peterson <jmatt@jmatt.org>
2015-09-09 10:50:38 -0700

    PEP8 Compliance.
    	modified:   scripts/user_verify.py

--- scripts/user_verify.py
+++ scripts/user_verify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import getpass
+import sys
 
 import libsaas
 
@@ -36,13 +37,28 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--user', required=True,
                         help='Github user to verify.')
-    parser.add_argument('-p', '--password', required=True,
+    parser.add_argument('-p', '--password', required=False,
                         action=PasswordPromptAction, type=str,
                         help='Github user password.')
+    parser.add_argument('-k', '--key', required=False,
+                        help='Public key registered to Github.')
     parser.add_argument('-r', '--repo', required=True,
                         help='Repository to verify.')
     args = parser.parse_args()
-    print(auth.verify_user(args.user, args.password))
+    if not args.key and not args.password:
+        print("Either a password or public key is required.")
+        sys.exit(2)
+    if args.key:
+        print("Key authorized? => %s" %
+              str(auth.verify_key(args.user, args.key)))
+        print("Unable to verify repo, not implemented.")
+    else:
+        print("User authorized? => %s" %
+              str(auth.verify_user(args.user, args.password)))
+        print("Push permissions to repo? => %s" %
+              str(auth.verify_repo(args.user,
+                                   args.password,
+                                   args.repo)))
 
 
 if __name__ == '__main__':
